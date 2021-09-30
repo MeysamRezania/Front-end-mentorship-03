bfb3b9ea4808bd52f78592a537df712a47d1d8f1
Meysam Rezania <meysamrezania94@gmail.com>
2021-09-30 17:18:30 +0330

    add city live search with ajax

--- app/app.js
+++ app/app.js
@@ -1,11 +1,43 @@
+// select inputs
 const inputs = document.querySelectorAll('.home__card--input');
 
+// toggle inputs overlay
 inputs.forEach((input) => {
   input.addEventListener('blur', (event) => {
-    if(event.target.value !== '') {
-      event.target.nextElementSibling.classList.add('invisible');
+    if (event.target.value !== '') {
+      event.target.parentElement
+        .querySelector('span')
+        .classList.add('invisible');
     } else {
-      event.target.nextElementSibling.classList.remove('invisible');
+      event.target.parentElement
+        .querySelector('span')
+        .classList.remove('invisible');
     }
-  })
-})
+  });
+});
+
+// live city query
+
+const locationInput = document.getElementById('location');
+const datalist = locationInput.nextElementSibling; //select the datalist
+
+const fetchCities = (apiUrl) => fetch(apiUrl); //fetch data from given url
+
+locationInput.addEventListener('input', (event) => {
+  if (event.target.value.trim().length < 3) return;
+  datalist.innerHTML = ''; // clear datalist
+  const city = event.target.value.trim();
+  const url = 'https://api.teleport.org/api/cities/?search=' + city;
+  
+  fetchCities(url) // returns a response with promise 
+    .then((response) => response.json()) // returns parsed result
+    .then((result) =>
+      result._embedded['city:search-results'].slice(0, 5) // picking first 5 results which are more relevant
+      .forEach((item) => {
+      const datalistOption = document.createElement('option');
+      datalistOption.value = item['matching_full_name'];
+      datalist.append(datalistOption);  
+      })
+    );
+});
+
